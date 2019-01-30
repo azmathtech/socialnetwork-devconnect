d6a1673c41b7401b03bcaf52dbc4b895cad230c1
Michael Evans <azmathtech@gmail.com>
2019-01-30 14:53:44 -0700

    register, login and logout functionality

---
+++ client/src/actions/authActions.js
@@ -0,0 +1,71 @@
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
+
+import { GET_ERRORS, SET_CURRENT_USER } from './types';
+import setAuthToken from '../utils/setAuthToken';
+
+// Register User
+export const registerUser = (userData, history) => dispatch => {
+  axios
+    .post('/api/users/register', userData)
+    .then(res => history.push('/login'))
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Login - Get User Token
+export const loginUser = (userData, history) => dispatch => {
+  axios
+    .post('/api/users/login', userData)
+    .then(res => {
+      //Save to localstorage
+      const { token } = res.data;
+      // Set token to localstorage
+      localStorage.setItem('jwtToken', token);
+      // Set token to Auth header
+      setAuthToken(token);
+      // Decode token to get user data
+      const decoded = jwt_decode(token);
+      // Set current user
+      dispatch(setCurrentUser(decoded));
+      history.push('/dashboard');
+    })
+    .catch(err =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      })
+    );
+};
+
+// Set logged in user
+export const setCurrentUser = decoded => {
+  return {
+    type: SET_CURRENT_USER,
+    payload: decoded
+  };
+};
+
+// Log User out
+export const logoutUser = history => dispatch => {
+  // Remove token from localStorage
+  localStorage.removeItem('jwtToken');
+  // Remove auth header for future requests
+  setAuthToken(false);
+  // Set the current user to {} which will also set isAuthenticated to false
+  dispatch(setCurrentUser({}));
+  // Return logged out user to homepage
+  history.push('/');
+};
+
+//testing stuff
+// return {
+//   type: TEST_DISPATCH,
+//   payload: userData
+// };
+
+// .then(res => console.log(res.data))
---
+++ client/src/components/auth/Register.js
@@ -0,0 +1,171 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { withRouter } from 'react-router-dom';
+//import axios from 'axios';
+import classnames from 'classnames';
+import { connect } from 'react-redux';
+
+import { registerUser } from '../../actions/authActions';
+
+class Register extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      password2: '',
+      errors: {}
+    };
+  }
+
+  //Depricated
+  // componentWillRecieveProps(nextProps) {
+  //   if (nextProps.errors) {
+  //     this.setState({ errors: nextProps.errors });
+  //   }
+  // }
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (nextProps.errors !== prevState.errors) {
+      return { errors: nextProps.errors };
+    } else return null;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.errors !== prevState.errors) {
+      this.setState({ errors: prevProps.errors });
+    }
+  }
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push('/dashboard');
+    }
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    const newUser = {
+      name: this.state.name,
+      email: this.state.email,
+      password: this.state.password,
+      password2: this.state.password2
+    };
+
+    this.props.registerUser(newUser, this.props.history);
+
+    // console.log(newUser); //testing
+    // axios
+    //   .post('/api/users/register', newUser)
+    //   .then(res => console.log(res.data))
+    //   .catch(err => this.setState({ errors: err.response.data }));
+    // // .catch(err => console.log(err.response.data));
+  };
+
+  render() {
+    const { errors } = this.state;
+
+    // const { user } = this.props.auth;
+    // {user ? user.name : null}
+
+    return (
+      <div>
+        <div className="register">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-8 m-auto">
+                <h1 className="display-4 text-center">Sign Up</h1>
+                <p className="lead text-center">
+                  Create your DevConnector account
+                </p>
+                <form noValidate onSubmit={this.onSubmit}>
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      className={classnames('form-control form-control-lg', {
+                        'is-invalid': errors.name
+                      })}
+                      placeholder="Name"
+                      name="name"
+                      value={this.state.name}
+                      onChange={this.onChange}
+                    />
+                    <div className="invalid-feedback">{errors.name}</div>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      className={classnames('form-control form-control-lg', {
+                        'is-invalid': errors.email
+                      })}
+                      placeholder="Email Address"
+                      name="email"
+                      value={this.state.email}
+                      onChange={this.onChange}
+                    />
+                    <div className="invalid-feedback">{errors.email}</div>
+                    <small className="form-text text-muted">
+                      This site uses Gravatar so if you want a profile image,
+                      use a Gravatar email
+                    </small>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className={classnames('form-control form-control-lg', {
+                        'is-invalid': errors.password
+                      })}
+                      placeholder="Password"
+                      name="password"
+                      value={this.state.password}
+                      onChange={this.onChange}
+                    />
+                    <div className="invalid-feedback">{errors.password}</div>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="password"
+                      className={classnames('form-control form-control-lg', {
+                        'is-invalid': errors.password2
+                      })}
+                      placeholder="Confirm Password"
+                      name="password2"
+                      value={this.state.password2}
+                      onChange={this.onChange}
+                    />
+                    <div className="invalid-feedback">{errors.password2}</div>
+                  </div>
+                  <input
+                    type="submit"
+                    className="btn btn-info btn-block mt-4"
+                  />
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+Register.propTypes = {
+  registerUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { registerUser }
+)(withRouter(Register));
